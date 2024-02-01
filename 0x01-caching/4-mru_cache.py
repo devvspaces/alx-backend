593f5767e13f59215e9b7998147f9889f8c0fe0a
Netrobe Ubuntu <netrobepy@gmail.com>
2024-02-01 05:28:10 +0100

    update

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,54 +1,39 @@
-#!/usr/bin/python3
-""" Basic Cache module
+#!/usr/bin/env python3
+"""Most Recently Used caching module.
 """
 
-BaseCaching = __import__('base_caching').BaseCaching
+from collections import OrderedDict
+from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """Basic caching implementation using MRU policy"""
-
+    """Implementation of MRU cache
+    """
     def __init__(self):
         """Initialize
         """
         super().__init__()
-        self.rank = 0
-        self.queue = dict()
-
-    def find_mru_key(self):
-        """ Find the most recently used key
-        """
-        most = 0
-        key = None
-        for k, v in self.queue.items():
-            if v > most:
-                key = k
-                most = v
-        return key
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """ Add an item in the cache
+        """Adds an item in the cache by key.
+        if key or item is none disregard
         """
         if key is None or item is None:
             return
-
-        self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            rm_key = self.find_mru_key()
-            del self.cache_data[rm_key]
-            del self.queue[rm_key]
-            print(f"DISCARD: {rm_key}")
-
-        self.queue[key] = self.rank
-        self.rank += 1
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                mru_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", mru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key
+        """Retrieves an item by key.
+        If key does not exist return None
         """
-        if key is None or key not in self.cache_data.keys():
-            return None
-        value = self.cache_data.get(key)
-        if value:
-            self.queue[key] = self.rank
-            self.rank += 1
-        return value
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
